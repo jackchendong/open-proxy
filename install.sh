14f01d72b407dde59883336175dba6b24d9937d9
gfw-breaker <lixiaoming0513@gmail.com>
2018-12-02 02:12:12 +0800

    -

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # auth : gfw-breaker
 
+rpm -ihv http://installrepo.kaltura.org/releases/kaltura-release.noarch.rpm
+yum install -y kaltura-nginx
+
 rm -fr /etc/nginx/conf.d/*
 
 mkdir -p /usr/local/nginx/content/cache
+mkdir -p /usr/share/nginx/html
 
 server_ip=$(ifconfig | grep "inet addr" | sed -n 1p | cut -d':' -f2 | cut -d' ' -f1)
 
@@ -14,7 +18,8 @@ done
 cp common/* /etc/nginx/
 cp sites/* /etc/nginx/conf.d
 
+mv /etc/init.d/kaltura-nginx /etc/init.d/nginx
+
 chkconfig nginx on
 service nginx restart
-
 
